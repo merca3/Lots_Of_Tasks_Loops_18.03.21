cc9aacc6085433188c33ebc0f78fa8572a317bee
Katrina M <merca3@inbox.lv>
2021-03-18 21:16:16 +0200

    Update main4.js

--- js/main4.js
+++ js/main4.js
@@ -1,42 +1,13 @@
-// 4. Write a program to find the perfect numbers between 1 and 500. 
-// The perfect numbers between 1 to 500 are:
-// 6
-// 28
-// 496
-
-// int main() {
-
-//     int number = 1, sum = 0;
-
-//     while (number <= 1000) {
-
-//         sum = 0;
-//         for (int i = 1; i < number; i++) {
-
-//             if (number % i == 0) {
-//                 sum += i;
-//             }
-//         }
-
-//         if (sum == number) {
-//             printf("%d is perfect\n", number);
-//         }
-//         number++;
-//     }
-//     return 0;
-// }
-
-
-// let counter = 1;
-// while (counter <= 500) {
-//     let sum = 0;
-//     for (let i = 1; i < counter; i++) {
-//         if (counter % i == 0 && counter !== i) {
-//             sum += i;
-//         }
-//     }
-//     if (sum = counter) {
-//         console.log(counter);
-//     }
-//     counter++;
-// }
+let counter = 1;
+while (counter <= 500) {
+    let sum = 0;
+    for (let i = 1; i < counter; i++) {
+        if (counter % i == 0 && counter !== i) {
+            sum += i;
+        }
+    }
+    if (sum == counter) {
+        console.log(`${counter} is a perfect number`);
+    }
+    counter++;
+};
