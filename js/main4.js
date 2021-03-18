3bea0074b608f77b2aa07ce135382dc45966d190
Katrina M <merca3@inbox.lv>
2021-03-18 21:04:39 +0200

    Update main4.js

--- js/main4.js
+++ js/main4.js
@@ -27,16 +27,16 @@
 // }
 
 
-let counter = 1;
-while (counter <= 500) {
-    let sum = 0;
-    for (let i = 1; i < counter; i++) {
-        if (counter % i == 0 && counter !== i) {
-            sum += i;
-        }
-    }
-    if (sum = counter) {
-        console.log(counter);
-    }
-    counter++;
-}
+// let counter = 1;
+// while (counter <= 500) {
+//     let sum = 0;
+//     for (let i = 1; i < counter; i++) {
+//         if (counter % i == 0 && counter !== i) {
+//             sum += i;
+//         }
+//     }
+//     if (sum = counter) {
+//         console.log(counter);
+//     }
+//     counter++;
+// }
